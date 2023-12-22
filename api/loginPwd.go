ed03d20c1cc8dbfe2314385d035c5c4b22e25b32
xufeixiang <xufeixiang@ainanjing.org.cn>
2023-12-22 15:52:32 +0800

    fix: resolve pwd login not use username.

--- api/loginPwd.go
+++ api/loginPwd.go
@@ -9,7 +9,7 @@ const PWD = "password"
 func init() {
 	// 通过密码登录
 	authActionMap[PWD] = func(authForm *AuthForm) (user *object.User, err error) {
-		user, err = object.CheckUserPassword(authForm.Phone, authForm.Password)
+		user, err = object.CheckUserPassword(authForm.Username, authForm.Password)
 		if err != nil {
 			return nil, err
 		}
