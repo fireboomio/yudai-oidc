b65a22acfa16faa8d560175206ce5f40eafbe6e6
xufeixiang <xufeixiang@ainanjing.org.cn>
2024-03-13 18:53:56 +0800

    fix: resolve miss config.yaml cause error.

--- object/setting.go
+++ object/setting.go
@@ -124,10 +124,7 @@ func Init() (err error) {
 	viper.SetConfigName("config") // 指定配置文件名（不带后缀）
 	viper.AddConfigPath("./conf") // 指定查找配置文件的路径（这里使用相对路径）
 	// 读取配置信息
-	if err = viper.ReadInConfig(); err != nil {
-		fmt.Printf("viper.ReadInConfig failed, err: %v\n", err)
-		return
-	}
+	_ = viper.ReadInConfig()
 
 	// 把读取到的配置信息反序列化到 Conf 变量中
 	if err = viper.Unmarshal(&Conf); err != nil {
